e59986f4722f7d2978676b0490c74405bf139c44
emmnavmoj <emmnavmoj@PRN5-10369385>
2025-12-17 19:31:51 +0100

    En este último commit, ya se completo la clase principal en la cuál llamara todo lo que hemos realizado durante el proyecto

--- src/mvc/MVC.java
+++ src/mvc/MVC.java
@@ -4,6 +4,10 @@
  */
 package mvc;
 
+import controlador.Controlador;
+import modelo.Modelo;
+import vista.Vista;
+
 /**
  *
  * @author emmnavmoj
@@ -14,7 +18,19 @@ public class MVC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Aqui llamaremos todo para ejecutar lo que queremos de manera correcta
+        // Instaciamos los objetos
+        Modelo mod = new Modelo();
+        Vista view = new Vista();
+        
+        // Llamaremos al controlador y le enviaremos los parametros que hemos creado
+        Controlador ctrl = new Controlador(view, mod);
+        
+        // Llamamos el métdo iniciar de la clase controlador
+        ctrl.iniciar();
+        
+        // Establecemos a la vista que ya se puede ver o ya puede ser visible el JFrame
+        view.setVisible(true);
     }
     
 }
